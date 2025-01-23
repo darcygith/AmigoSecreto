4311c6a790405078b53e14470ca3226f3f18565a
darcygith <darcynestudios@gmail.com>
2025-01-22 22:05:35 -0500

    se crea una lista donde se visualiza los nombres ingresados por el input, se muestra por la consola

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let listaU =[];
+
+function agregarAmigo(){
+    let usuarios = document.getElementById('amigo').value;
+    
+    listaU.push(usuarios);   
+    console.log(listaU);
+    
+}
